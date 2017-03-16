7acc28026073ea3b18dfe5905a6be9b64bd303a0
Teus Benschop <teusjannette@gmail.com>
2017-03-16 07:40:28 +0100

    i18n

--- config.h
+++ config.h
@@ -29,7 +29,7 @@
 #define HAVE_LIBPROC 1
 
 /* Define whether to compile for Linux */
-#define HAVE_LINUX 1
+/* #undef HAVE_LINUX */
 
 /* Define whether to compile for Mac */
 /* #undef HAVE_MAC */
@@ -83,7 +83,7 @@
 #define PACKAGE_NAME "bibledit"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 5.0.124"
+#define PACKAGE_STRING "bibledit 5.0.125"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -92,13 +92,13 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.0.124"
+#define PACKAGE_VERSION "5.0.125"
 
 /* Define to 1 if you have the ANSI C header files. */
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "5.0.124"
+#define VERSION "5.0.125"
 
 /* Define whether to compile on Windows */
 /* #undef WIN32 */

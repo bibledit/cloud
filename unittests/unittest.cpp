87fa2ff3866e94a390c3745962dd9465a3275540
Teus Benschop <teusjannette@gmail.com>
2017-03-18 20:44:38 +0100

    Updated unit tests
    Clean up file name patterns.

--- unittests/unittest.cpp
+++ unittests/unittest.cpp
@@ -131,7 +131,7 @@ int main (int argc, char **argv)
   // Automated Unit Tests Start Removing Code Here.
   
   refresh_sandbox (true);
-  test_filter_mail ();
+  test_editone_logic ();
   exit (0);
   
   // The next line is a signature for automated unit testing: Do not change it.

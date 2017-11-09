3831e3a631273944b58bbb17684bde337febbc11
Teus Benschop <teusjannette@gmail.com>
2017-11-09 14:58:18 +0100

    Fixed: RSS dates are one month ahead
    This fixes https://github.com/bibledit/cloud/issues/69

--- filter/date.cpp
+++ filter/date.cpp
@@ -228,18 +228,18 @@ string filter_date_day_rfc822 (int day)
 
 string filter_date_month_rfc822 (int month)
 {
-  if (month ==  0) return "Jan";
-  if (month ==  1) return "Feb";
-  if (month ==  2) return "Mar";
-  if (month ==  3) return "Apr";
-  if (month ==  4) return "May";
-  if (month ==  5) return "Jun";
-  if (month ==  6) return "Jul";
-  if (month ==  7) return "Aug";
-  if (month ==  8) return "Sep";
-  if (month ==  9) return "Oct";
-  if (month == 10) return "Nov";
-  if (month == 11) return "Dec";
+  if (month ==  1) return "Jan";
+  if (month ==  2) return "Feb";
+  if (month ==  3) return "Mar";
+  if (month ==  4) return "Apr";
+  if (month ==  5) return "May";
+  if (month ==  6) return "Jun";
+  if (month ==  7) return "Jul";
+  if (month ==  8) return "Aug";
+  if (month ==  9) return "Sep";
+  if (month == 10) return "Oct";
+  if (month == 11) return "Nov";
+  if (month == 12) return "Dec";
   return "";
 }
 

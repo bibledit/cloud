d182c4744e74071559c5aa10fa95aec7649b822d
Teus Benschop <teusjannette@gmail.com>
2017-12-23 18:15:36 +0100

    Exclude more file extensions from the obfuscation check
    https://github.com/bibledit/cloud/issues/120

--- obfuscate/check.sh
+++ obfuscate/check.sh
@@ -3,5 +3,5 @@
 # Checks for certain words whether they occur in the filenames.
 
 # Do not look in the source code because it is to be removed after build.
-find . -name "*bible*" ! -name "*.cpp" ! -name "*.h"
+find . -name "*bible*" ! -name "*.cpp" ! -name "*.h" ! -name "*.Po" ! -name "*.o" ! -name "*.xccheckout" ! -name "*.xcscheme"
 find . -name "*Bible*"

274a09a1851b6c9b7d07d549567606b0d45d92ce
Teus Benschop <teusjannette@gmail.com>
2018-02-20 14:39:51 +0100

    Uses system-supplied mbedTLS headers, if available
    https://github.com/bibledit/cloud/issues/168

--- filter/url.cpp
+++ filter/url.cpp
@@ -29,13 +29,13 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #ifndef HAVE_CLIENT
 #include <curl/curl.h>
 #endif
-#include "mbedtls/net.h"
-#include "mbedtls/debug.h"
-#include "mbedtls/ssl.h"
-#include "mbedtls/entropy.h"
-#include "mbedtls/ctr_drbg.h"
-#include "mbedtls/error.h"
-#include "mbedtls/certs.h"
+#include <mbedtls/net.h>
+#include <mbedtls/debug.h>
+#include <mbedtls/ssl.h>
+#include <mbedtls/entropy.h>
+#include <mbedtls/ctr_drbg.h>
+#include <mbedtls/error.h>
+#include <mbedtls/certs.h>
 #ifdef HAVE_WINDOWS
 #include <direct.h>
 #include <io.h>

02ac471d315c998d255759354a5a70eeaed14d4c
Teus Benschop <teusjannette@gmail.com>
2019-01-20 17:32:02 +0100

    Refactor unit tests

--- unittests/dev.cpp
+++ unittests/dev.cpp
@@ -33,55 +33,7 @@ void test_dev () // Todo
   
   string bible = "phpunit";
   
-  // Test plain text and notes for export to Quick Bible.
-  {
-    string usfm = R"(
-\id GEN
-\c 1
-\p
-\v 1 This is verse one\x + Xref 1\x*\f + Note 1\f*.
-\v 2 This is verse two\f + Note 2\f*\x + Xref 2\x*.
-\v 3 This is verse three\fe + Endnote 3\fe*.
-    )";
-    Filter_Text filter_text = Filter_Text (bible);
-    filter_text.initializeHeadingsAndTextPerVerse (false);
-    filter_text.addUsfmCode (usfm);
-    filter_text.run (styles_logic_standard_sheet ());
-
-    map <int, string> output = filter_text.getVersesText ();
-    map <int, string> standard = {
-      {1, "This is verse one."},
-      {2, "This is verse two."},
-      {3, "This is verse three."},
-    };
-    evaluate (__LINE__, __func__, standard, output);
-
-    int n = 5;
-    int size = filter_text.notes_plain_text.size();
-    evaluate (__LINE__, __func__, n, size);
-    if (size == n) {
-      evaluate (__LINE__, __func__, "1", filter_text.notes_plain_text[0].first);
-      evaluate (__LINE__, __func__, "1", filter_text.notes_plain_text[1].first);
-      evaluate (__LINE__, __func__, "2", filter_text.notes_plain_text[2].first);
-      evaluate (__LINE__, __func__, "2", filter_text.notes_plain_text[3].first);
-      evaluate (__LINE__, __func__, "3", filter_text.notes_plain_text[4].first);
-      evaluate (__LINE__, __func__, "Xref 1", filter_text.notes_plain_text[0].second);
-      evaluate (__LINE__, __func__, "Note 1", filter_text.notes_plain_text[1].second);
-      evaluate (__LINE__, __func__, "Note 2", filter_text.notes_plain_text[2].second);
-      evaluate (__LINE__, __func__, "Xref 2", filter_text.notes_plain_text[3].second);
-      evaluate (__LINE__, __func__, "Endnote 3", filter_text.notes_plain_text[4].second);
-    }
-    
-    evaluate (__LINE__, __func__, 3, filter_text.verses_text_note_positions.size());
-    evaluate (__LINE__, __func__, {}, filter_text.verses_text_note_positions [0]);
-    evaluate (__LINE__, __func__, {17, 17}, filter_text.verses_text_note_positions [1]);
-    evaluate (__LINE__, __func__, {17, 17}, filter_text.verses_text_note_positions [2]);
-    evaluate (__LINE__, __func__, {19}, filter_text.verses_text_note_positions [3]);
-    evaluate (__LINE__, __func__, {}, filter_text.verses_text_note_positions [4]);
-  }
   
-  
-  return; // Todo
   // Test check on matching markers.
   {
     string usfm = R"(

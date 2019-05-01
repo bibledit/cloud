67a299d11747526cc4fec7c369126ef422090d7a
Teus Benschop <teusjannette@gmail.com>
2019-05-01 08:59:16 +0200

    Cleanup

--- email/send.cpp
+++ email/send.cpp
@@ -264,8 +264,8 @@ string email_send (string to_mail, string to_name, string subject, string body, 
    * part of the Transport Layer Security protection by setting the
    * CURLOPT_SSL_VERIFYPEER and CURLOPT_SSL_VERIFYHOST options to 0 (false).
    */
-  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); // Todo
-  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); // Todo
+  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+  // curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
   /*
    * That is, in general, a bad idea. It is still better than sending your
    * authentication details in plain text though.

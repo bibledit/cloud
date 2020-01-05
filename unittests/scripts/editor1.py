dc8ed9582c420932300162dab613266bb6cd330f
Teus Benschop <teusjannette@gmail.com>
2020-01-05 13:15:32 +0100

    Test scripts Bible save problem
    https://github.com/bibledit/cloud/issues/340

--- unittests/scripts/editor1.py
+++ unittests/scripts/editor1.py
@@ -2,22 +2,32 @@
 
 from selenium import webdriver
 import time
- 
+import os
+
+
+input("Start a client at localhost:8080 and press Enter to continue")
+
 driver1 = webdriver.Firefox()
 driver1.implicitly_wait(2)
 driver1.set_window_position(30, 30)
-driver1.set_window_size(800,600)
+driver1.set_window_size(900,900)
 
 driver2 = webdriver.Firefox()
 driver2.implicitly_wait(2)
-driver2.set_window_position(900, 30)
-driver2.set_window_size(800,600)
+driver2.set_window_position(1100, 30)
+driver2.set_window_size(900,900)
 
-driver1.get("https://bibledit.org:8081/editone/index")
-driver2.get("https://bibledit.org:8081/editone/index")
+driver1.get("http://localhost:8080/editone/index")
+driver2.get("http://localhost:8080/editone/index")
 
-input("Do your thing in the browsers and press Enter to continue")
+input("Navigate to the Sample Bible Genesis 1:1 and press Enter to continue")
 
-driver1.switch_to.active_element.send_keys("abc")
-time.sleep(0.5)
-driver2.switch_to.active_element.send_keys("xyz")
+while True:
+    print ("Type abc in first browser")
+    driver1.switch_to.active_element.send_keys("abc")
+    print ("Waiting shortly")
+    time.sleep(0.5)
+    print ("Type xyz in second browser")
+    driver2.switch_to.active_element.send_keys("xyz")
+    input("Press Enter to go through the cycle again")
+
--- unittests/scripts/editor1.sh
+++ unittests/scripts/editor1.sh
@@ -1,45 +1,5 @@
 #!/bin/sh
 
-echo Start two Firefox browsers
-PORT1=4444
-PORT2=5555
-
-geckodriver --port $PORT1 &
-PID1=$!
-geckodriver --port $PORT2 &
-PID2=$!
-
-sleep 1
-
-curl -d '{"capabilities": {"alwaysMatch": {"acceptInsecureCerts": true}}}' http://localhost:$PORT1/session | tee out.txt
-# This outputs something like this:
-# {"value": {"sessionId":"e46f5e73-7dd2-7b42-9fd5-0a6483933097","capabilities":{"acceptInsecureCerts":true,"browserName":"firefox","browserVersion":"71.0","capabilities":{"desiredCapabilities":{"acceptInsecureCerts":true}},"moz:accessibilityChecks":false,"moz:buildID":"20191202093317","moz:headless":false,"moz:processID":32663,"moz:profile":"/var/folders/z9/lc690hv133qfzcy5cz65jcp00000gn/T/rust_mozprofile.KUJw4HCgDqHX","moz:shutdownTimeout":60000,"moz:useNonSpecCompliantPointerOrigin":false,"moz:webdriverClick":true,"pageLoadStrategy":"normal","platformName":"mac","platformVersion":"19.2.0","rotatable":false,"setWindowRect":true,"strictFileInteractability":false,"timeouts":{"implicit":0,"pageLoad":300000,"script":30000},"unhandledPromptBehavior":"dismiss and notify"}}}
-SESSION1=`cat out.txt | cut -d \" -f6`
-echo
-
-curl -d '{"capabilities": {"alwaysMatch": {"acceptInsecureCerts": true}}}' http://localhost:$PORT2/session | tee out.txt
-SESSION2=`cat out.txt | cut -d \" -f6`
-rm out.txt
-echo
-
-sleep 1
-
-curl -d '{"url": "https://bibledit.org:8081/editone/index"}' http://localhost:$PORT1/session/$SESSION1/url
-curl -d '{"url": "https://bibledit.org:8081/editone/index"}' http://localhost:$PORT2/session/$SESSION2/url
-# curl http://localhost:$PORT1/session/$SESSION1/url
-
-sleep 3
-echo
-echo Session 1 is $SESSION1 on port $PORT1
-echo Session 2 is $SESSION2 on port $PORT2
-echo Press any key to close the sessions and browsers
-read -n 1
-curl -X DELETE http://localhost:$PORT1/session/$SESSION1
-curl -X DELETE http://localhost:$PORT2/session/$SESSION2
-sleep 3
-kill -9 $PID1
-kill -9 $PID2
-killall geckodriver
-echo
-echo
+FILE=`ls -t /tmp/bibledit-dev/bibles/Sample/1/1 | head -1`
+cat /tmp/bibledit-dev/bibles/Sample/1/1/$FILE | head
 

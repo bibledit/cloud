a4512630cb99944a8932680efd4c8f610cc8bdab
Teus Benschop <teusjannette@gmail.com>
2020-07-17 10:28:19 +0200

    Fuller footnotes entry
    https://github.com/bibledit/cloud/issues/404

--- edit/index.js
+++ edit/index.js
@@ -888,7 +888,7 @@ Section for the notes.
 var editorInsertedNotesCount = 0;
 
 
-function applyNotesStyle (style)
+function applyNotesStyle (style) // Todo
 {
   if (!editorWriteAccess) return;
 
@@ -914,12 +914,24 @@ function applyNotesStyle (style)
 
   // Append note text to notes section:
   // <p class="b-f"><span class="i-notebody1">1</span> + .</p>
+  // <p class="b-f"><span class="i-notebody1">1</span> + <span class="i-fr">1.1 </span><span class="i-fk">keyword </span><span class="i-ft">Footnote text.</span></p>
   var length = quill.getLength ();
   quill.insertText (length, "\n", "paragraph", style, "user");
   quill.insertText (length, caller, "character", "notebody" + noteId, "user");
   length++;
-  quill.insertText (length, " + .", "character", "", "user");
-  
+  quill.insertText (length, " + ", "character", "", "user");
+  length += 3;
+  var reference = editorNavigationChapter + "." + editorNavigationVerse;
+  quill.insertText (length, reference, "character", "fr", "user");
+  length += reference.length;
+  quill.insertText (length, " ", "character", "fk", "user");
+  length++;
+  quill.insertText (length, "keyword", "character", "fk", "user");
+  length += 7;
+  quill.insertText (length, " ", "character", "ft", "user");
+  length++;
+  quill.insertText (length, "Text.", "character", "ft", "user");
+
   editorInsertedNotesCount++;
   
   editorActiveStylesFeedback ();
--- editone/index.js
+++ editone/index.js
@@ -737,7 +737,7 @@ function oneverseCaretMovedTimeoutStart ()
 var oneverseInsertedNotesCount = 0;
 
 
-function oneverseApplyNotesStyle (style)
+function oneverseApplyNotesStyle (style) // Todo
 {
   if (!oneverseEditorWriteAccess) return;
 
@@ -764,11 +764,23 @@ function oneverseApplyNotesStyle (style)
 
   // Append note text to notes section:
   // <p class="b-f"><span class="i-notebody1">1</span> + .</p>
+  // <p class="b-f"><span class="i-notebody1">1</span> + <span class="i-fr">1.1 </span><span class="i-fk">keyword </span><span class="i-ft">Footnote text.</span></p>
   var length = quill.getLength ();
   quill.insertText (length, "\n", "paragraph", style, "user");
   quill.insertText (length, caller, "character", "notebody" + noteId, "user");
   length++;
-  quill.insertText (length, " + .", "character", "", "user");
+  quill.insertText (length, " + ", "character", "", "user");
+  length += 3;
+  var reference = oneverseNavigationChapter + "." + oneverseNavigationVerse;
+  quill.insertText (length, reference, "character", "fr", "user");
+  length += reference.length;
+  quill.insertText (length, " ", "character", "fk", "user");
+  length++;
+  quill.insertText (length, "keyword", "character", "fk", "user");
+  length += 7;
+  quill.insertText (length, " ", "character", "ft", "user");
+  length++;
+  quill.insertText (length, "Text.", "character", "ft", "user");
 
   oneverseInsertedNotesCount++;
   

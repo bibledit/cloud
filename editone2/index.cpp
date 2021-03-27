50ec1e3db1cd5420f277e3269b357be621628b1e
Teus Benschop <teusjannette@gmail.com>
2021-03-27 14:24:35 +0100

    Guest accounts within main Indonesian Instance
    https://github.com/bibledit/cloud/issues/527

--- editone2/index.cpp
+++ editone2/index.cpp
@@ -63,7 +63,7 @@ bool editone2_index_acl (void * webserver_request)
 }
 
 
-string editone2_index (void * webserver_request)
+string editone2_index (void * webserver_request) // Todo
 {
   Webserver_Request * request = (Webserver_Request *) webserver_request;
   
@@ -90,8 +90,9 @@ string editone2_index (void * webserver_request)
     }
     if (selected_bible.empty ()) {
       // No Bible selected yet: Create the Indonesian Sample Bible and take that.
-      demo_create_sample_bible ();
-      selected_bible = demo_sample_bible_name ();
+      string user = request->session_logic ()->currentUser ();
+      filter_indonesian_create_mytranslation (user);
+      selected_bible = filter_indonesian_terjemahanku_mytranslation_name (user);
     }
     request->database_config_user()->setBible (selected_bible);
   }

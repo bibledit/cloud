167a045ca019bf56b370a62a861f4f16df49a385
Teus Benschop <teusjannette@gmail.com>
2021-05-20 18:37:19 +0200

    cleanup
    https://github.com/bibledit/cloud/issues/640

--- assets/checkbox.js
+++ assets/checkbox.js
@@ -17,9 +17,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 
-var checkboxLastInput; // Todo out.
+var checkboxLastInput;
 
-function checkbox (input, url, var1, var2) { // Todo out eventually.
+function checkbox (input, url, var1, var2) {
   // Store this input.
   checkboxLastInput = input;
   // The URL where to POST to.

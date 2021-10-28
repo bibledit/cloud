6dd00f3caa7ef03176c392b86cae80cb07ac8a9c
Teus Benschop <teusjannette@gmail.com>
2021-10-28 19:39:34 +0200

    Show the commandline output for installing a SWORD resource.
    https://github.com/bibledit/cloud/issues/695

--- sword/logic.cpp
+++ sword/logic.cpp
@@ -314,8 +314,10 @@ void sword_logic_install_module (string source_name, string module_name)
   
 #else
   
-  string out_err;
-  filter_shell_run ("cd " + sword_path + "; installmgr --allow-internet-access-and-risk-tracing-and-jail-or-martyrdom --allow-unverified-tls-peer -ri \"" + source_name + "\" \"" + module_name + "\"", out_err);
+  string out_err; // Todo add command and log it.
+  string command = "cd " + sword_path + "; installmgr --allow-internet-access-and-risk-tracing-and-jail-or-martyrdom --allow-unverified-tls-peer -ri \"" + source_name + "\" \"" + module_name + "\"";
+  Database_Logs::log (command);
+  filter_shell_run (command, out_err);
   sword_logic_log (out_err);
   
 #endif

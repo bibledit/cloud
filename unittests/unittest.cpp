2229db91e17bf178eb0c67209e7fa9177e233627
Teus Benschop <teusjannette@gmail.com>
2022-01-02 15:58:01 +0100

    Upgrade UTF-8 library
    https://github.com/bibledit/cloud/issues/724

--- unittests/unittest.cpp
+++ unittests/unittest.cpp
@@ -143,7 +143,7 @@ int main ([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
   
   refresh_sandbox (false);
   test_dev ();
-  test_database_privileges ();
+  test_string ();
   refresh_sandbox (false);
   exit (0);
   

3dfeb9acd96f5a12a7cb3e597ec5d2be1e7dad99
Teus Benschop <teusjannette@gmail.com>
2022-01-02 21:06:21 +0100

    Fix compile error on ARM
    https://github.com/bibledit/cloud/issues/719

--- mimetic098/codec/base64.cxx
+++ mimetic098/codec/base64.cxx
@@ -14,19 +14,19 @@ const char Base64::sEncTable[] =
     "0123456789+/=";
 
 const char Base64::sDecTable[] = {
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
-        -1,-1,-1,62,-1,-1,-1,63,52,53,
-        54,55,56,57,58,59,60,61,-1,-1,
-        -1, eq_sign, -1,-1,-1, 0, 1, 2, 3, 4, 
+        static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),
+        static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),
+        static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),
+        static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),
+        static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),62,static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),63,52,53,
+        54,55,56,57,58,59,60,61,static_cast<char>(-1),static_cast<char>(-1),
+        static_cast<char>(-1), eq_sign, static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1), 0, 1, 2, 3, 4,
          5, 6, 7, 8, 9,10,11,12,13,14,
         15,16,17,18,19,20,21,22,23,24,
-        25,-1,-1,-1,-1,-1,-1,26,27,28,
+        25,static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),static_cast<char>(-1),26,27,28,
         29,30,31,32,33,34,35,36,37,38,
         39,40,41,42,43,44,45,46,47,48,
-        49,50,51,-1
+        49,50,51,static_cast<char>(-1)
 };
 
 const int Base64::sDecTableSz = sizeof(Base64::sDecTable) / sizeof(char);

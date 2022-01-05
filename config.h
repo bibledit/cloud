84551bbd6a60e1de2bf4eaac78f905a56e3842ef
Teus Benschop <teusjannette@gmail.com>
2022-01-05 17:38:33 +0100

    version bump

--- config.h
+++ config.h
@@ -86,7 +86,7 @@
 #define PACKAGE_PREFIX_DIR "NONE"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "bibledit 5.0.974"
+#define PACKAGE_STRING "bibledit 5.0.975"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "bibledit"
@@ -95,7 +95,7 @@
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "5.0.974"
+#define PACKAGE_VERSION "5.0.975"
 
 /* Define to 1 if all of the C90 standard headers exist (not just the ones
    required in a freestanding environment). This macro is provided for
@@ -103,7 +103,7 @@
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "5.0.974"
+#define VERSION "5.0.975"
 
 /* Define whether to compile on Windows */
 /* #undef WIN32 */

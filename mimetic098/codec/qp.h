235594f68f1de24e7c314bfe6bb4c65e222b7235
Teus Benschop <teusjannette@gmail.com>
2022-02-08 18:27:26 +0100

    Fix compile warning

--- mimetic098/codec/qp.h
+++ mimetic098/codec/qp.h
@@ -243,7 +243,8 @@ class Decoder: public buffered_codec, public chainable_codec<Encoder>
         sNewline,
         sOtherChar
     };
-    size_t m_pos, m_maxlen;
+  [[maybe_unused]] size_t m_pos;
+  size_t m_maxlen;
 
 
     int m_state, m_nl;

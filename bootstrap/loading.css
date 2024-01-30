bb192119ffeb395b49eab09c53ac476c86854183
Teus Benschop <teusjannette@gmail.com>
2024-01-30 18:36:26 +0100

    Loader html / css
    https://github.com/bibledit/cloud/issues/920

---
+++ bootstrap/loading.css
@@ -0,0 +1,164 @@
+/*
+ Copyright (©) 2003-2024 Teus Benschop.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+
+@font-face {
+  font-family: "Esteban";
+  src: url(../fonts/Esteban-Regular.ttf);
+}
+@font-face {
+  font-family: "Montserrat Light";
+  src: url(../fonts/Montserrat-Light.ttf);
+}
+@font-face {
+  font-family: "Montserrat Regular";
+  src: url(../fonts/Montserrat-Regular.ttf);
+}
+@font-face {
+  font-family: "Montserrat Semi-Bold";
+  src: url(../fonts/Montserrat-SemiBold.ttf);
+}
+@font-face {
+  font-family: "Ezra SIL";
+  src: url(../fonts/SILEOT.ttf);
+}
+@font-face {
+  font-family: "Cardo";
+  src: url(../fonts/Cardo-Regular.ttf);
+}
+
+html {
+  height: 100%;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+
+body {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display:         flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+          flex-direction: column;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  font-family: 'Esteban', serif;
+}
+
+h1, h2, h3, h4, h5, h6,
+#name1, #name2, #name3,
+#topbar,
+#editorinnerheader,
+.center,
+.button {
+  font-family: "Montserrat Regular", sans-serif;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  margin: .5em 0;
+  font-weight: bold;
+  display: inline-block;
+}
+
+h1 {
+  font-size: 2em;
+}
+
+h2 {
+  font-size: 1.5em;
+}
+
+h3 {
+  font-size: 1.3em;
+}
+
+h4 {
+  font-size: 1.1em;
+}
+
+h5 {
+  font-size: .9em;
+}
+
+h6 {
+  font-size: .7em;
+}
+
+.splash {
+  display: -webkit-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display:         flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -webkit-flex-direction: column;
+      -ms-flex-direction: column;
+          flex-direction: column;
+  -webkit-box-flex: 1;
+  -webkit-flex-grow: 1;
+  -ms-flex-positive: 1;
+          flex-grow: 1;
+  padding: 5%;
+  border-radius: 5px;
+}
+
+.splash > * {
+  -webkit-box-flex: 1;
+  -webkit-flex-grow: 1;
+  -ms-flex-positive: 1;
+  flex-grow: 1;
+}
+
+#software-name {
+  font: 4.5rem "Montserrat Light", sans-serif;
+  background-color: #598fb2;
+  background-image: linear-gradient(35deg, #196394, #30739f, #598fb2);
+  background-image: -webkit-linear-gradient(35deg, #196394, #30739f, #598fb2);
+  background-size: 100%;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+}
+
+.centerbible {
+  -webkit-box-flex: 100;
+  -webkit-flex-grow: 100;
+  -ms-flex-positive: 100;
+          flex-grow: 100;
+  background-image: url(../pix/bibledit_logo.svg);
+  background-position: center center;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+
+.center {
+  text-align: center;
+}
+
+body {
+  background-color: #fff;
+}
---
+++ bootstrap/loading.html
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon" href="../pix/favicon.ico">
+<title>Bibledit</title>
+<link rel="stylesheet" type="text/css" href="loading.css?5.1.010">
+</head>
+<body>
+<div class="splash">
+<h3 id="software-name" class="center">....loading....</h3>
+<div class="centerbible"></div>
+</div>
+</div>
+</body>
+</html>

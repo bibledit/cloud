bc51157f5e17e8994f19ba72bab5598723bf8266
Teus Benschop <teusjannette@gmail.com>
2024-05-02 19:18:13 +0200

    Remove check on availability of std::stoi
    https://github.com/bibledit/cloud/issues/940

--- config.h
+++ config.h
@@ -52,9 +52,6 @@
 /* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define whether function 'stoi' is available */
-#define HAVE_STOI 1
-
 /* Define to 1 if you have the <strings.h> header file. */
 #define HAVE_STRINGS_H 1
 

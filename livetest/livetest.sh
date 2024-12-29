2cf8d3b1a28dc9b180100e4daf9130b5602883b1
Teus Benschop <teusjannette@gmail.com>
2024-12-29 18:52:29 +0100

    Bump copyright years

--- livetest/livetest.sh
+++ livetest/livetest.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Copyright (©) 2003-2024 Teus Benschop.
+# Copyright (©) 2003-2025 Teus Benschop.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- locale/README
+++ locale/README
@@ -1,4 +1,4 @@
-# Copyright (©) 2003-2024 Teus Benschop.
+# Copyright (©) 2003-2025 Teus Benschop.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by

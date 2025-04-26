65030e045709e7dcffa3285b179e4072bdef0a61
Teus Benschop <teusjannette@gmail.com>
2025-04-26 16:59:11 +0200

    Cleanup
    https://github.com/bibledit/cloud/issues/990

--- database/styles.h
+++ database/styles.h
@@ -35,51 +35,6 @@ bool has_write_access (const std::string& user, const std::string& sheet);
 } // End namespace styles
 
 
-namespace database::styles1 {
-
-struct Item
-{
-  std::string marker {};
-  std::string name {};
-  std::string info {};
-  std::string category {};
-  int type {0};
-  int subtype {0};
-  float fontsize {12.0f};
-  int italic {0};
-  int bold {0};
-  int underline {0};
-  int smallcaps {0};
-  int superscript {0};
-  int justification {0};
-  float spacebefore {0.0f};
-  float spaceafter {0.0f};
-  float leftmargin {0.0f};
-  float rightmargin {0.0f};
-  float firstlineindent {0.0f};
-  bool spancolumns {false};
-  std::string color {"#000000"};
-  bool print {false};
-  bool userbool1 {false};
-  bool userbool2 {false};
-  bool userbool3 {false};
-  int userint1 {0};
-  int userint2 {0};
-  int userint3 {0};
-  std::string userstring1 {};
-  std::string userstring2 {};
-  std::string userstring3 {};
-  std::string backgroundcolor {"#FFFFFF"};
-};
-
-
-std::map <std::string, std::string> get_markers_and_names (const std::string& sheet);
-std::vector <std::string> get_markers (const std::string& sheet);
-Item get_marker_data (const std::string& sheet, const std::string& marker);
-
-} // End namespace styles1
-
-
 namespace database::styles2 {
 
 void add_marker (const std::string& sheet, const std::string& marker);

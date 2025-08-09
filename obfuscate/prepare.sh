219323b0438ddc3fc0a77c869d9d585f09b5dcd8
Teus Benschop <teusjannette@gmail.com>
2025-08-09 17:43:41 +0200

    Simplify error handling in prepare.sh script
    Replaced manual error checks after each command with 'set -e' to exit on any error automatically. This streamlines the script and reduces repetitive code.
    https://github.com/bibledit/cloud/issues/1026

--- obfuscate/prepare.sh
+++ obfuscate/prepare.sh
@@ -20,6 +20,10 @@
 # Checks for certain words whether they occur in the filenames and in the data.
 
 
+# Exit script on error.
+set -e
+
+
 # Definitions for what to change.
 Bibledit=Scripturedit
 bibledit=scripturedit
@@ -29,9 +33,7 @@ bible=scripture
 
 echo Changing the program name and installation location to ${bibledit}/${Bibledit}
 sed -i.bak "s/bibledit/$bibledit/g" configure.ac
-if [ $? -ne 0 ]; then exit; fi
 sed -i.bak "s/Bibledit/$Bibledit/g" configure.ac
-if [ $? -ne 0 ]; then exit; fi
 # Remove backup file(s).
 rm *.bak
 
@@ -43,17 +45,12 @@ echo Updating and renaming bibledit.desktop to ${bibledit}/${Bibledit}
 # Exec=bibledit
 # Update those.
 sed -i.bak "s/Bibledit/${Bibledit}/g" bibledit.desktop
-if [ $? -ne 0 ]; then exit; fi
 sed -i.bak "s/Bible/${Bible}/g" bibledit.desktop
-if [ $? -ne 0 ]; then exit; fi
 sed -i.bak "s/bibledit/${bibledit}/g" bibledit.desktop
-if [ $? -ne 0 ]; then exit; fi
 # Change the name of the file also.
 mv "bibledit.desktop" "${bibledit}.desktop"
-if [ $? -ne 0 ]; then exit; fi
 # Change the builder that uses this desktop file.
 sed -i.bak "s/bibledit.desktop/$bibledit.desktop/g" Makefile.am
-if [ $? -ne 0 ]; then exit; fi
 # Remove backup file(s).
 rm *.bak
 
@@ -61,21 +58,17 @@ rm *.bak
 # Remove the binary "bibledit" itself.
 # The obfuscated binary will have a different name.
 rm -f bibledit
-if [ $? -ne 0 ]; then exit; fi
 
 
 # Remove the internationalization file.
 # It is not needed.
 rm -f locale/bibledit.pot
-if [ $? -ne 0 ]; then exit; fi
 
 
 echo Renaming the bibles folder where to store the Bibles to ${bible}s
 mv bibles ${bible}s
-if [ $? -ne 0 ]; then exit; fi
 # Update the references to this folder in the code.
 sed -i.bak "s/\"bibles\"/\"${bible}s\"/g" database/bibles.cpp setup/logic.cpp
-if [ $? -ne 0 ]; then exit; fi
 # Remove backup file(s).
 rm database/*.bak
 rm setup/*.bak
@@ -83,10 +76,8 @@ rm setup/*.bak
 
 echo Renaming the databases/config/bible folder where to store the Bibles configuration data
 mv databases/config/bible databases/config/${bible}
-if [ $? -ne 0 ]; then exit; fi
 # Update the references to this folder in the code.
 sed -i.bak "s/\"bible\"/\"${bible}\"/g" database/config/bible.cpp
-if [ $? -ne 0 ]; then exit; fi
 # Remove backup file(s).
 rm database/config/*.bak
 
@@ -104,28 +95,21 @@ rm *.bak
 # The wildcard * is the well-known one.
 # The #1 matches the first wildcard, #2 the second wildcard, and so on.
 # mmv ";*Bible*" "#1#2$Bible#3"
-# if [ $? -ne 0 ]; then exit; fi
 
 
 # Deal with "freebible.html" and where it is called.
 # mmv ";*freebible*" "#1#2free$bible#3"
-# if [ $? -ne 0 ]; then exit; fi
 # find . -name "*.html" -print0 | xargs -0 sed -i '' -e "s/freebible/free$bible/g"
-# if [ $? -ne 0 ]; then exit; fi
 
 
 # Deal with "bibledit.xpm" and where it is called.
 # mmv ";*bibledit.xpm*" "#1#2$bibledit.xpm#3"
-# if [ $? -ne 0 ]; then exit; fi
 # sed -i '' "s/bibledit.xpm/$bibledit.xpm/g" Makefile.am executable/bibledit.cpp
-# if [ $? -ne 0 ]; then exit; fi
 
 
 # Deal with "bibledit.png" and where it is called.
 # mmv ";*bibledit.png*" "#1#2$bibledit.png#3"
-# if [ $? -ne 0 ]; then exit; fi
 # sed -i '' "s/bibledit.png/$bibledit.png/g" Makefile.am setup/index.html
-# if [ $? -ne 0 ]; then exit; fi
 
 
 echo Ready preparing $Bibledit

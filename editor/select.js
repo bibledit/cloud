5c0a1b4738d97e2da882bd16d131ab69a24a24d0
Teus Benschop <teusjannette@gmail.com>
2025-12-04 18:26:58 +0100

    Replace jQuery keydown handler with native event listener
    Switched from using jQuery's keydown event handler to the native addEventListener for improved performance and reduced dependency on jQuery.
    https://github.com/bibledit/cloud/issues/1054

--- editor/select.js
+++ editor/select.js
@@ -18,7 +18,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 
 document.addEventListener("DOMContentLoaded", function(e) {
-  $ (window).on ("keydown", editSwitchKeyHandler);
+  window.addEventListener("keydown", editSwitchKeyHandler);
 });
 
 

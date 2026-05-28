87a8815e7d0d0cb4e0522a83d58ed273450476f3
Teus Benschop <teusjannette@gmail.com>
2026-05-28 17:55:39 +0200

    Modernize code
    https://github.com/bibledit/cloud/issues/1071

--- consistency/poll.cpp
+++ consistency/poll.cpp
@@ -25,26 +25,27 @@
 #include <database/temporal.h>
 
 
-std::string consistency_poll_url ()
+std::string consistency_poll_url()
 {
-  return "consistency/poll";
+    return "consistency/poll";
 }
 
 
-bool consistency_poll_acl (Webserver_Request& webserver_request)
+bool consistency_poll_acl(Webserver_Request& webserver_request)
 {
-  return roles::access_control (webserver_request, roles::translator);
+    return roles::access_control(webserver_request, roles::translator);
 }
 
 
-std::string consistency_poll (Webserver_Request& webserver_request)
+std::string consistency_poll(Webserver_Request& webserver_request)
 {
-  const int id = filter::string::convert_to_int (webserver_request.query ["id"]);
-  const Consistency_Logic consistency_logic(webserver_request, id);
-  const std::string response = consistency_logic.response ();
-  if (response != database::temporal::get_value(id, "response")) {
-    database::temporal::set_value(id, "response", response);
-    return response;
-  }
-  return std::string();
+    const int id = filter::string::convert_to_int(webserver_request.query["id"]);
+    const Consistency_Logic consistency_logic(webserver_request, id);
+    if (const std::string response = consistency_logic.response();
+        response != database::temporal::get_value(id, "response"))
+    {
+        database::temporal::set_value(id, "response", response);
+        return response;
+    }
+    return {};
 }

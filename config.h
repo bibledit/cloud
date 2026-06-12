498fe4e8781772b0715cdd7d62b7d7a88d47f5d7
Teus Benschop <teusjannette@gmail.com>
2026-06-12 18:46:27 +0200

    Version bum
    https://github.com/bibledit/cloud/issues/1095

--- config.h
+++ config.h
@@ -20,8 +20,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #define VERSION_MAJOR 5
 #define VERSION_MINOR 1
-#define VERSION_PATCH 048
-#define VERSION "5.1.048"
+#define VERSION_PATCH 050
+#define VERSION "5.1.050"
 #define HAVE_LIBMBEDTLS
 /* #undef HAVE_LIBPROC */
 /* #undef HAVE_EXECINFO */
